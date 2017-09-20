67eecc9ca74c5ba3d0d96dcf10385819b4eee03d
曾智 <service@unnnn.cn>
2017-09-20 23:55:58 +0800

    增加拦截器的处理。

--- dreup-web/src/main/java/com/unnnn/webapi/filter/ActionHandlerInterceptor.java
+++ dreup-web/src/main/java/com/unnnn/webapi/filter/ActionHandlerInterceptor.java
@@ -13,17 +13,17 @@ import javax.servlet.http.HttpServletResponse;
 public class ActionHandlerInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
-        System.out.println(">>>MyInterceptor1>>>>>>>在请求处理之前进行调用（Controller方法调用之前）");
+        System.out.println(">>>MyInte1rceptor1>>>>>>>在请求处理之前进行调用（Controller方法调用之前）");
         return false;
     }
 
     @Override
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-        System.out.println(">>>MyInterceptor1>>>>>>>请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）");
+        System.out.println(">>>MyInt1erceptor1>>>>>>>请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-        System.out.println(">>>MyInterceptor1>>>>>>>在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）");
+        System.out.println(">1>>MyInterceptor1>>>>>>>在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）");
     }
 }
